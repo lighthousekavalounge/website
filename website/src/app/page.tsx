8e2405b267522be8e93ecf2781c375f1339018fd
Samuel Torres <rilla@MacBook-Pro.local>
2023-11-17 20:06:43 -0700

    Initial Commit

---
+++ website/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Lighthouse Kava Lounge</h1>
+        <p>Coming Soon...</p>
+      </div>
+    </main>
+  );
+}
